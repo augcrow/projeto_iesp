d89b4c23028c96282c19f984e5f301a0bb992dea
augcrow <augcrow@gmail.com>
2018-06-02 12:02:54 -0300

    AulaPHP

---
+++ src/Domain/Model/HabilidadeTecnica.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lab05usuario14
+ * Date: 02/06/2018
+ * Time: 11:40
+ */
+
+namespace Domain\Model;
+
+
+class HabilidadeTecnica
+{
+
+    /**
+     * @var int
+     */
+    private $idHabilidadeTecnica;
+    /**
+     * @var sttring
+     */
+    private $descricao;
+
+
+
+
+}
